5f7aca5329355b3ab05c447dcf4f629c8c697233
毒牙 <1052363946@qq.com>
2019-10-25 00:19:11 +0800

    鄭家router

--- routes/duya.js
+++ routes/duya.js
@@ -4,6 +4,10 @@ module.exports = [
 		path: "/",
 		handler: (req, h) => {
 			return "Welcome to duya's world!!!"
+		},
+		config: {
+			tags: ["api", "tests"],
+			description: "測試接口的返回"
 		}
 	}
 ]
