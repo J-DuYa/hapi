57012a7062ce6fb5e4b37b996659f40a067d16e3
毒牙 <1052363946@qq.com>
2019-10-30 09:21:48 +0800

    ❤ add: 集成數據庫

---
+++ config/db_config.js
@@ -0,0 +1,8 @@
+//db_config.js
+module.exports = {
+    database: 'h_api',//库名
+    username: 'root',//用户名
+    password: '123456',//密码
+    host: 'localhost',//数据库地址
+    dialect: 'mysql'//数据库类型
+}
